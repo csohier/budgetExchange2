66152916d24f1ffe2d7a2ec48ebc550d8c7c1f89
Thomas Chan <thomas8df8df@hotmail.com>
2020-03-26 16:56:28 +1100

    Created Login Activity and Login xml, but currently unable to open it for some reason.

--- app/src/main/java/com/example/budgetexchange/MainActivity.java
+++ app/src/main/java/com/example/budgetexchange/MainActivity.java
@@ -1,20 +1,91 @@
 package com.example.budgetexchange;
 
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.example.budgetexchange.ui.login.LoginActivity;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText userNameInput;
+    private TextInputEditText passwordInput;
+    private CheckBox rememberMe;
+    private Button loginBtn;
+    private Button signUpBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        userNameInput = findViewById(R.id.userNameInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        rememberMe = findViewById(R.id.rememberMe);
+
+        //open user dashboard
+        ClickLogin();
+
+        //open register xml
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickSignUp();
+            }
+        });
+
+
     }
+
+    //This is method for doing operation of check login
+    private void ClickLogin() {
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (userNameInput.getText().toString().trim().isEmpty()) {
+
+                    Snackbar snackbar = Snackbar.make(view, "Please fill out these fields", Snackbar.LENGTH_LONG);
+                    View snackbarView = snackbar.getView();
+                    snackbarView.setBackgroundColor(getResources().getColor(R.color.red));
+                    snackbar.show();
+                    txtInLayoutUsername.setError("Username should not be empty");
+                } else {
+                    //Here you can write the codes for checking username
+                }
+
+                if (passwordInput.getText().toString().trim().isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(view, "Please fill out these fields", Snackbar.LENGTH_LONG);
+                    View snackbarView = snackbar.getView();
+                    snackbarView.setBackgroundColor(getResources().getColor(R.color.red));
+                    snackbar.show();
+                    txtInLayoutPassword.setError("Password should not be empty");
+                } else {
+                    //Here you can write the codes for checking password
+                }
+
+                if (rememberMe.isChecked()) {
+                    //Here you can write the codes if box is checked
+                } else {
+                    //Here you can write the codes if box is not checked
+                }
+
+            }
+
+        });
+
+    }
+
+
 }
