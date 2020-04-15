96a3fabdb22d3c8cb147c6647b12d1b3800ade4f
Thomas Chan <thomas8df8df@hotmail.com>
2020-04-15 12:47:58 +1000

    Added Currency API, but need to set base and required currency

--- app/src/main/java/com/example/budgetexchange/CurrencyService.java
+++ app/src/main/java/com/example/budgetexchange/CurrencyService.java
@@ -1,6 +1,10 @@
 package com.example.budgetexchange;
+import com.example.budgetexchange.Entities.Currency;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface CurrencyService {
+    @GET("latest?base=AUD")
+    Call<Currency> getBase ();
 }
