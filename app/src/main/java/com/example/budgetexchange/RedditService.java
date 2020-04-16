f8f5d7e304f624cd0ed83780d3bece8a371f4d08
chibiuruha <50163204+chibiuruha@users.noreply.github.com>
2020-04-16 10:54:05 +1000

    Expense class

---
+++ app/src/main/java/com/example/budgetexchange/RedditService.java
@@ -0,0 +1,16 @@
+package com.example.budgetexchange;
+
+import com.example.budgetexchange.Entities.Currency;
+import com.example.budgetexchange.Reddit.DataDetail;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RedditService {
+    @GET("/r/budgetfood/top.json?limit=30")
+    Call<List<DataDetail>> getFood();
+
+
+}
