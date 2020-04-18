c14fbb4a03b1a5e69e27030008b13acf969ef959
Thomas Chan <thomas8df8df@hotmail.com>
2020-04-18 17:36:55 +1000

    placed expense stuff in a folder "expense"

--- app/src/main/java/com/example/budgetexchange/CurrencyConverter/Rates.java
+++ app/src/main/java/com/example/budgetexchange/CurrencyConverter/Rates.java
@@ -1,4 +1,4 @@
-package com.example.budgetexchange.Entities;
+package com.example.budgetexchange.CurrencyConverter;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
